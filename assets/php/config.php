72d456fd481571a8bab25565d7f3a8ee9e2c94f3
AsiriMevan <mevansenanayakems@gmail.com>
2022-04-26 11:07:43 +0530

    User Systerm First

---
+++ assets/php/config.php
@@ -0,0 +1,22 @@
+<?php
+
+    class Database{
+        private $dsn = "mysql:host=localhost:3306;dbname=db_user_system";
+        private $dbuser = "root";
+        private $dbpass = "";
+
+        public $conn;
+
+        public function __construct(){
+            try
+            {
+                $this->conn = new PDO($this->dsn,$this->dbuser,$this->dbpass);
+            }
+            catch(PDOException $e)
+            {
+                echo 'Error : '.$e->getMessage();
+            }
+            return $this->conn;
+        }
+    }
+?>
